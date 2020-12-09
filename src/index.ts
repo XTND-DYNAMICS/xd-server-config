5d521c0f6a9a94a71ab2d04a88c0c8f42934bcfb
Peter Andreas Moelgaard <gitkraken@petermolgaard.com>
2020-12-09 13:03:23 +0700

    handling optional namespace & environment specific files better

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,24 @@ export class ServerConfigurationLoader {
         options = _.assign({}, this.options, options);
 
         const optionsDir: string = path.join(<string>options.dirname, <string>options.basePath);
-
         const optionsBaseFilePath: string = path.join(optionsDir, <string>options.namespace);
-        const optionsBase = require(optionsBaseFilePath);
         const optionsEnvFilePath: string = path.join(optionsBaseFilePath + '.' + process.env.NODE_ENV);
-        const optionsEnv = require(optionsEnvFilePath);
+        
+        let optionsBase;
+        try {
+            optionsBase = require(optionsBaseFilePath);
+        } catch (err) {
+            optionsBase = {};
+        }
+
+        let optionsEnv;
+        try {
+            optionsEnv = require(optionsEnvFilePath);
+        } catch (err) {
+            optionsEnv = {};
+        }
 
         const result = _.merge({}, optionsBase, optionsEnv);
-
         return result;
     }
 }
